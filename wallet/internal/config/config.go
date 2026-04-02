7b480880acd508fff955eefff872c369c7a1e6a2
Luiz Philipe Xavier Lobo Filho <lp.lobofilho@gmail.com>
2026-04-02 01:14:17 -0300

    feat: add .gitignore file

---
+++ wallet/internal/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+type DBConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Name     string
+}
+
+func GetDBConfig() DBConfig {
+	return DBConfig{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Name:     os.Getenv("DB_NAME"),
+	}
+}
+
+func (c DBConfig) DSN() string {
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", c.User, c.Password, c.Host, c.Port, c.Name)
+}
